1e9582fea87686163dc80826a9548a67d60560da
Max <mdiazn@uni.pe>
2022-01-10 18:22:44 -0500

    clase 22 context header cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
 import Menu from '@components/Menu';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
+import AppContext from '../context/AppContext';
 
 const Header = () => {
 	const [toggle, setToggle] = useState(false);
+	const { state:{ cart } } = useContext(AppContext);
 
 	const handlerToggle = () =>{
 		setToggle(!toggle);
@@ -44,7 +46,7 @@ const Header = () => {
 					</li>
 					<li className="navbar-shopping-cart">
 						<img src={shoppingCart} alt="shopping cart" />
-						<div>2</div>
+						{cart.length > 0 ? <div>{cart.length}</div>:null}
 					</li>
 				</ul>
 			</div>
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -6,9 +6,7 @@ import btAddToCart from '@icons/bt_add_to_cart.svg';
 const ProductItem = ({ product }) => {
 	const { addToCart } = useContext(AppContext);
 
-	const handlerClick = ({item})=>{
-		addToCart(item);
-	}
+	const handlerClick = item=>{addToCart(item);}
 	return (
 		<div className="ProductItem">
 			<img src={product.images[0]} alt={product.title} />
